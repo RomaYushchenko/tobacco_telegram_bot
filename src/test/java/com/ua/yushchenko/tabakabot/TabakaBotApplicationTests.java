1e0eb637d65f13eca50b12dc1d6e01d0b4793fcf
RomaYushchenko <yushenkoromaf7@gmail.com>
2024-05-13 01:31:40 +0300

    Revert "[COMMON] Split one telegram bots service into two telegram bots services"
    This reverts commit 72a2447e30776002b942c0c1cfcc806d890b3b2c.

---
+++ src/test/java/com/ua/yushchenko/tabakabot/TabakaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.yushchenko.tabakabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabakaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
